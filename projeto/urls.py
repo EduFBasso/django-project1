bf78f4c8a35bdb5c3d5661020ec7da170bf45cdc
Eduardo Figueiredo Basso <edu_fabric@outlook.com>
2025-11-22 18:37:16 -0300

    Funções home, sobre e contato enviadas para views do app recipes para executar um passo por vez, importadas para urls.py do projeto

--- projeto/urls.py
+++ projeto/urls.py
@@ -2,22 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-
-
-# HTTP REQUEST <-> HTTP RESPONSE
-def home(request):
-    return HttpResponse("Home")
-
-def sobre(request):
-    return HttpResponse("Sobre")
-
-def contato(request):
-    return HttpResponse("Contato")
+from recipes.views import home, sobre, contato
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), # Rota: / "apenas domínio raiz ou ip do servidor: http://127.0.0.1:8000/"
-    path('sobre/', sobre), # Rota: /sobre/
-    path('contato/', contato), # Rota: /contato/
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
