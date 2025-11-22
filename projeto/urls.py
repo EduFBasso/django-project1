a2ffcf8ad0ae4c3f0e3b4f4e18d0cabb66e810ea
Eduardo Figueiredo Basso <eduardofigueiredobasso@MacBook-Air-de-Eduardo.local>
2025-11-22 15:22:32 -0300

    Exemplo simples Request <-> response em urls.py na pasta do projeto

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,7 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+# HTTP REQUEST <-> HTTP RESPONSE
+def sobre_view(request):
+    return HttpResponse("Página Sobre")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', sobre_view),
 ]
