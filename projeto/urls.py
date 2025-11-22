1c22a8b7577339d09d9a90009a61e0d69b308966
Eduardo Figueiredo Basso <edu_fabric@outlook.com>
2025-11-22 18:20:47 -0300

    Create  app recipes: python manage.py startapp recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -5,10 +5,19 @@ from django.http import HttpResponse
 
 
 # HTTP REQUEST <-> HTTP RESPONSE
-def sobre_view(request):
-    return HttpResponse("Página Sobre")
+def home(request):
+    return HttpResponse("Home")
+
+def sobre(request):
+    return HttpResponse("Sobre")
+
+def contato(request):
+    return HttpResponse("Contato")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', sobre_view), # Rota raiz do site (assume a página sobre)
+    path('', home), # Rota: / "apenas domínio raiz ou ip do servidor: http://127.0.0.1:8000/"
+    path('sobre/', sobre), # Rota: /sobre/
+    path('contato/', contato), # Rota: /contato/
 ]
