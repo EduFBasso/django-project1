1f24645dff98bf62f2d928913f77aa59c35d5f24
Eduardo Figueiredo Basso <edu_fabric@outlook.com>
2025-11-22 21:25:59 -0300

    Renderização page home: urls.py (projeto) -> urls.py (app recipes) -> render home.html (dentro de recipes/template). Obs. Em settings precisa incluir o app recipes em INSTALLED_APPS para reconhecer a pasta, e ler home.html.

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Home 1")
+    return render(request, 'home.html') # RENDERIZA template home.html 
+# (mágica do Django -> Não esqueça de especificar em settings.py em APPS_INSTALLED 
+# a app 'recipes' para o Django encontrar o template)
 
 def sobre(request):
     return HttpResponse("Sobre")
